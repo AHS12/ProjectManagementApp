e7db09ec8865d3390b8cbb4a1d2422d1a7b4c248
Md Azizul Hakim <ahslucky12@gmail.com>
2019-06-09 09:37:57 +0600

    issue: cant use local css so now using cdn

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="{{ asset('css.helper.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css.helper.css') }}"> --}}
+    {{-- <link href="{{URL:: asset('public/css/helper.css') }}" rel="stylesheet" type="text/css"> --}}
+
+    {{-- {!! Html::style('css/helper.css') !!} --}}
+
+    {{-- <link rel="stylesheet" type="text/css" href="public/css/helper.css"> --}}
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -21,6 +26,15 @@
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/css/mdb.min.css" rel="stylesheet"> --}}
     {{-- datatable --}}
+
+
+
+    {{-- <style>
+        .is-completed {
+            text-decoration: line-through;
+        }
+    </style> --}}
+    <link rel="stylesheet" href="https://ahs12.000webhostapp.com/MyCDN/helper.css">
 
     <title>@yield('title','ProjectManagementApp')</title>
 
